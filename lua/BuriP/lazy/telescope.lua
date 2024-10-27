5627c2fab88298451c17b7922de6320ef0e73724
Arturo <arturo.perez@paylogic.eu>
2024-10-27 16:34:36 +0100

    dadbod and lsp

--- lua/BuriP/lazy/telescope.lua
+++ lua/BuriP/lazy/telescope.lua
@@ -83,7 +83,7 @@ return {
 		vim.keymap.set("n", "<leader>/", function()
 			-- You can pass additional configuration to Telescope to change the theme, layout, etc.
 			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-				winblend = 40,
+				winblend = 50,
 				previewer = true,
 			}))
 		end, { desc = "[/] Fuzzily search in current buffer" })
