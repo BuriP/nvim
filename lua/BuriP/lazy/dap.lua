d7b598a82321adc6447f6616399b85379a534c6a
Arturo <arturo.perez@paylogic.eu>
2025-03-01 23:47:19 +0100

    dap

--- lua/BuriP/lazy/dap.lua
+++ lua/BuriP/lazy/dap.lua
@@ -1,80 +1,100 @@
 return {
-	{ "rcarriga/nvim-dap-ui" },
-	{ "mfussenegger/nvim-dap-python" },
 	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"leoluz/nvim-dap-go",
-			"mfussenegger/nvim-dap-python",
-			"rcarriga/nvim-dap-ui",
-			"theHamsta/nvim-dap-virtual-text",
-			"nvim-neotest/neotest",
-			"nvim-neotest/nvim-nio",
-			"williamboman/mason.nvim",
-			"jayp0521/mason-nvim-dap.nvim",
+		{
+			"mfussenegger/nvim-dap",
+			dependencies = {
+				"mfussenegger/nvim-dap-python",
+				"nvim-neotest/nvim-nio",
+				"rcarriga/nvim-dap-ui",
+				"theHamsta/nvim-dap-virtual-text",
+			},
+			config = function()
+				local dap = require("dap")
+				local dapui = require("dapui")
+				local dap_python = require("dap-python")
+
+				require("dapui").setup({})
+				dap.configurations.dapui_breakpoints = {}
+				require("nvim-dap-virtual-text").setup({
+					commented = true, -- Show virtual text alongside comment
+				})
+
+				dap_python.setup("~/.local/share/nvim/mason/packages/debugpy/venv/bin/python")
+
+				vim.fn.sign_define("DapBreakpoint", {
+					text = "",
+					texthl = "DiagnosticSignError",
+					linehl = "",
+					numhl = "",
+				})
+
+				vim.fn.sign_define("DapBreakpointRejected", {
+					text = "", -- or "❌"
+					texthl = "DiagnosticSignError",
+					linehl = "",
+					numhl = "",
+				})
+
+				vim.fn.sign_define("DapStopped", {
+					text = "", -- or "→"
+					texthl = "DiagnosticSignWarn",
+					linehl = "Visual",
+					numhl = "DiagnosticSignWarn",
+				})
+
+				-- Automatically open/close DAP UI
+				dap.listeners.after.event_initialized["dapui_config"] = function()
+					dapui.open()
+				end
+
+				local opts = { noremap = true, silent = true }
+
+				-- Toggle breakpoint
+				vim.keymap.set("n", "<leader>dpb", function()
+					dap.toggle_breakpoint()
+				end, opts)
+
+				-- Continue / Start
+				vim.keymap.set("n", "<leader>dpc", function()
+					dap.continue()
+				end, opts)
+
+				-- Step Over
+				vim.keymap.set("n", "<leader>dpo", function()
+					dap.step_over()
+				end, opts)
+
+				-- Step Into
+				vim.keymap.set("n", "<leader>dpi", function()
+					dap.step_into()
+				end, opts)
+
+				-- Step Out
+
+				-- Keymap to terminate debugging
+				vim.keymap.set("n", "<leader>dpq", function()
+					require("dap").terminate()
+				end, opts)
+
+				-- Toggle DAP UI
+				vim.keymap.set("n", "<leader>dpu", function()
+					dapui.toggle()
+				end, opts)
+			end,
 		},
-		config = function()
-			-- Require nvim-dap and dap-python
-			local dap = require("dap")
-			local dap_python = require("dap-python")
+	},
 
-			-- Configure dap-python
-
-			--Configurinig python adapter
-			dap.adapters.python = {
-				type = "executable",
-				command = "/Users/burip/.local/share/nvim/mason/packages/debugpy/venv/bin/python3", -- Ensure it’s the right path
-				args = { "-m", "debugpy.adapter" },
-			}
-
-			-- Example Python DAP configuration
-			dap.configurations.python = {
-				{
-					type = "python",
-					request = "launch",
-					name = "Launch file",
-					program = "${file}",
-					console = "integratedTerminal",
-					env = {
-						DJANGO_SETTINGS_MODULE = "paylogic.backoffice.settings",
-					},
-				},
-			}
-
-			-- Go configuration (nvim-dap-go handles configurations automatically)
-			dap.adapters.go = {
-				type = "executable",
-				port = "${port}",
-				executable = {
-					command = "dlv",
-					args = { "dap", "-l", "127.0.0.1:${port}" },
-				},
-			}
-
-			-- Keymaps
-			vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
-			vim.keymap.set("n", "<space>?", function()
-				require("dapui").eval(nil, { enter = true })
-			end)
-			vim.keymap.set("n", "<space>dpc", dap.continue)
-			vim.keymap.set("n", "<F2>", dap.step_into)
-			vim.keymap.set("n", "<F3>", dap.step_over)
-			vim.keymap.set("n", "<F4>", dap.step_out)
-			vim.keymap.set("n", "<F5>", dap.step_back)
-			vim.keymap.set("n", "<F13>", dap.restart)
-
-			-- DAP UI auto open/close
-			local ui = require("dapui")
-			ui.setup()
-			dap.listeners.after.event_initialized["dapui_config"] = function()
-				ui.open()
-			end
-			dap.listeners.before.event_terminated["dapui_config"] = function()
-				ui.close()
-			end
-			dap.listeners.before.event_exited["dapui_config"] = function()
-				ui.close()
-			end
+	{
+		"mfussenegger/nvim-dap-python",
+		ft = "python",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+			"rcarriga/nvim-dap-ui",
+			"nvim-neotest/nvim-nio",
+		},
+		config = function(_, opts)
+			local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+			require("dap-python").setup(path)
 		end,
 	},
 }
