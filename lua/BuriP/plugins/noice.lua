d21a23a7bca1b43ec1ce2e039f02b9f150055656
Arturo <arturo.perez@paylogic.eu>
2025-05-02 10:38:53 +0200

    noice cmdline

--- lua/BuriP/plugins/noice.lua
+++ lua/BuriP/plugins/noice.lua
@@ -7,6 +7,9 @@ return {
 	},
 	config = function()
 		require("noice").setup({
+			cmdline = {
+				view = "cmdline",
+			},
 			lsp = {
 				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 				override = {
@@ -17,7 +20,7 @@ return {
 			},
 			-- you can enable a preset for easier configuration
 			presets = {
-				bottom_search = false, -- use a classic bottom cmdline for search
+				bottom_search = true, -- use a classic bottom cmdline for search
 				command_palette = true, -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
 				inc_rename = false, -- enables an input dialog for inc-rename.nvim
